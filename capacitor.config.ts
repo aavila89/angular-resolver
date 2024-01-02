1a2e3191bd584c9a94f73a166dfb61413dd5f94b
aavila89 <90415322+aavila89@users.noreply.github.com>
2024-01-02 09:16:54 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'example-angular-resolver',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
